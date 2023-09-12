59074095631106d1961158347eea1e0d21e39172
Henri de Boutray <henrideboutray@hotmail.fr>
2023-09-12 10:06:56 +0200

    try to make it compile

--- build.py
+++ build.py
@@ -4,12 +4,16 @@ from typing import List
 
 from setuptools import Extension, Distribution
 
+# so that copy_extensions_to_source is in cython_build_ext (complicated import process)
+import setuptools.command.build_ext
+# it doesn't even works :(
+from setuptools.command.build_ext import build_ext
+
 from Cython.Build import cythonize
 from Cython.Distutils.build_ext import new_build_ext as cython_build_ext
 
 SOURCE_DIR = Path("poetry_compile_test")  # replace SRC with the root of your source
 BUILD_DIR = Path("cython_build")
-
 
 def get_extension_modules() -> List[Extension]:
     """Collect all .py files and construct Setuptools Extensions"""
@@ -21,7 +25,7 @@ def get_extension_modules() -> List[Extension]:
         module_path = py_file.with_suffix("")
 
         # Convert path to module name
-        module_path = str(module_path).replace("/", ".")
+        module_path = str(module_path).replace("/", ".").replace("\\", ".")
 
         extension_module = Extension(
             name=module_path,
@@ -45,7 +49,7 @@ def cythonize_helper(extension_modules: List[Extension]) -> List[Extension]:
         annotate=False,
 
         # Parallelize our build
-        nthreads=multiprocessing.cpu_count() * 2,
+        # nthreads=multiprocessing.cpu_count() * 2,
 
         # Tell Cython we're using Python 3. Becomes default in Cython 3
         compiler_directives={"language_level": "3"},
@@ -54,22 +58,20 @@ def cythonize_helper(extension_modules: List[Extension]) -> List[Extension]:
         force=True,
     )
 
-print(get_extension_modules())
-
-# # Collect and cythonize all files
-# extension_modules = cythonize_helper(get_extension_modules())
+# Collect and cythonize all files
+extension_modules = cythonize_helper(get_extension_modules())
 
 
-# # Use Setuptools to collect files
-# distribution = Distribution({
-#     "ext_modules": extension_modules,
-#     "cmdclass": {
-#         "build_ext": cython_build_ext,
-#     },
-# })
+# Use Setuptools to collect files
+distribution = Distribution({
+    "ext_modules": extension_modules,
+    "cmdclass": {
+        "build_ext": build_ext,
+    },
+})
 
-# # Grab the build_ext command and copy all files back to source dir.
-# # Done so Poetry grabs the files during the next step in its build.
-# distribution.run_command("build_ext")
-# build_ext_cmd = distribution.get_command_obj("build_ext")
-# build_ext_cmd.copy_extensions_to_source()
+# Grab the build_ext command and copy all files back to source dir.
+# Done so Poetry grabs the files during the next step in its build.
+distribution.run_command("build_ext")
+build_ext_cmd = distribution.get_command_obj("build_ext")
+build_ext_cmd.copy_extensions_to_source()
